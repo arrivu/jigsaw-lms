3edc210334ceff5a2987fa740ef6efced5d2592d
alfredpl <alfred@arrivusystems.com>
2014-01-04 09:07:52 +0000

    capistrano-script changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server "162.243.117.179", :app, :web, :db, :primary => true
-set :deploy_to, "/var/deploy/capistrano/canvas"
+set :deploy_to, "/var/deploy/capistrano/jigsaw"
 set :branch,    "deploy"
 set :scm_passphrase, ""
 set :smart_lms_data_files, "#{deploy_to}/data/files"
